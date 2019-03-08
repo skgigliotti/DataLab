60b3a84dca1559d9dbadd8d3a84f7141ff6bd1a9
Sophia <sgigliotti@westmont.edu>
2019-03-07 16:10:30 -0800

    adding code

---
+++ dlab.c
@@ -0,0 +1,165 @@
+
+/* 
+ * upperBits - pads n upper bits with 1's
+ *  You may assume 0 <= n <= 32
+ *  Example: upperBits(4) = 0xF0000000
+ *  Legal ops: ! ~ & ^ | + << >>
+ *  Max ops: 10
+ *  Rating: 1
+ */
+int upperBits(int n) {
+   int y = !!n;
+   int x;
+   int neg1 = ~0;
+   y = y << 31;
+   y = y >> (n + neg1);
+
+   return y;
+}
+
+int isNotEqual(int x, int y) {
+   
+  return !(!(x ^ y));
+}
+
+/*
+ * isZero - returns 1 if x == 0, and 0 otherwise 
+ *   Examples: isZero(5) = 0, isZero(0) = 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 2
+ *   Rating: 1
+ */
+int isZero(int x) {
+  return !x;
+}
+
+
+/* 
+ * sign - return 1 if positive, 0 if zero, and -1 if negative
+ *  Examples: sign(130) = 1
+ *            sign(-23) = -1
+ *  Legal ops: ! ~ & ^ | + << >>
+ *  Max ops: 10
+ *  Rating: 2
+ */
+int sign(int x) {
+    int y = x >> 30;
+    int z = !!x;
+
+    return y|z; 
+}
+
+/* 
+ * isAsciiDigit - return 1 if 0x30 <= x <= 0x39 (ASCII codes for characters '0' to '9')
+ *   Example: isAsciiDigit(0x35) = 1.
+ *            isAsciiDigit(0x3a) = 0.
+ *            isAsciiDigit(0x05) = 0.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 15
+ *   Rating: 3
+ */
+int isAsciiDigit(int x) {
+  return 2;
+}
+
+/* 
+ * isNegative - return 1 if x < 0, return 0 otherwise 
+ *   Example: isNegative(-1) = 1.
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 6
+ *   Rating: 2
+ */
+int isNegative(int x) {
+  int y = 1;
+  y = y << 31;
+  
+  return !(!(x & y));
+}
+
+
+/*
+ * bitParity - returns 1 if x contains an odd number of 0's
+ *   Examples: bitParity(5) = 0, bitParity(7) = 1
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 20
+ *   Rating: 4
+ */
+int bitParity(int x) {
+  return 2;
+}
+
+/*
+ * ezThreeFourths - multiplies by 3/4 rounding toward 0,
+ *   Should exactly duplicate effect of C expression (x*3/4),
+ *   including overflow behavior.
+ *   Examples: ezThreeFourths(11) = 8
+ *             ezThreeFourths(-9) = -6
+ *             ezThreeFourths(1073741824) = -268435456 (overflow)
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 12
+ *   Rating: 3
+ */
+int ezThreeFourths(int x) {
+
+  //don't shift the first bit
+
+  int m = 1 << 31;
+  
+  m = (m & x);
+  
+  x = x + x + x;
+  x = x >> 2;
+  x = x ^ m;
+
+  return x;
+}
+
+/* 
+ * minusOne - return a value of -1 
+ *   Legal ops: ! ~ & ^ | + << >>
+ *   Max ops: 2
+ *   Rating: 1
+ */
+int minusOne(void) {
+  return ~0;
+}
+
+
+/* 
+ * float_f2i - Return bit-level equivalent of expression (int) f
+ *   for floating point argument f.
+ *   Argument is passed as unsigned int, but
+ *   it is to be interpreted as the bit-level representation of a
+ *   single-precision floating point value.
+ *   Anything out of range (including NaN and infinity) should return
+ *   0x80000000u.
+ *   Legal ops: Any integer/unsigned operations incl. ||, &&. also if, while
+ *   Max ops: 30
+ *   Rating: 4
+ * 01000000100001000000000000000000
+ * 01000000100100000000000000000000
+ * 01000000100110000000000000000000
+ */
+int float_f2i(unsigned uf) {
+  
+
+  return 2;
+}
+
+
+
+
+int main(int argc, char *argv[]){
+  //printf("%d \n", sign(4));
+  //printf("%d \n", sign(0));
+  //printf("%d \n", sign(-10));
+  printf("%d \n",ezThreeFourths(11));
+  printf("%d \n",ezThreeFourths(-9));
+  printf("%d \n",ezThreeFourths(1073741824));
+
+}
+
+
+
+
+
